48b0fd586170dfa3ca59f49b75ab9fecbadb46f9
ahmetsirel <ahmetsirel@gmail.com>
2018-11-08 21:51:08 +0300

    adding venv

---
+++ twitter.py
@@ -0,0 +1,19 @@
+# Import the necessary package to process data in JSON format
+import json
+import twitter
+
+
+
+# Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = 'YOUR ACCESS TOKEN"'
+ACCESS_SECRET = 'YOUR ACCESS TOKEN SECRET'
+CONSUMER_KEY = 'YOUR API KEY'
+CONSUMER_SECRET = 'ENTER YOUR API SECRET'
+
+oauth = twitter.OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+# Initiate the connection to Twitter REST API
+twitter = twitter.Twitter(auth=oauth)
+
+# Search for latest tweets about "#nlproc"
+twitter.search.tweets(q='#nlproc', result_type='recent', lang='en', count=10)
